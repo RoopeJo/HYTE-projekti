fb56312945b6f53a8b92cfcaa83cce2f010981d8
RoopeJo <roope.jokinen@metropolia.fi>
2026-01-15 11:52:05 +0200

    feat: implement basic REST API with Express
    - Added Express as a dependency and refactored the server setup.
    - Created endpoints for retrieving all items and a specific item by ID.
    - Implemented functionality to add new items to the list.
    - Added a test file for HTTP requests to interact with the API.
    - TODOs added for future implementation of PUT and DELETE routes.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import http from 'http';
+import express from 'express';
 const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to my REST API!');
+// Dummy data (nollautuu aina kun sovelluksen käynnistää uudellen)
+const items = [{ "id": 1, "name": "Omena" },
+               { "id": 2, "name": "Banaani" },
+               { "id": 3, "name": "Mansikka" }];
+
+//parsitaan json data pyynnnostä ja lisää request objekti
+app.use(express.json());
+
+// API root
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
 });
 
-server.listen(port, hostname, () => {
+// Get all items
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+// Get item based on id
+app.get('/items/:id', (req, res) => {
+  console.log('get item with id:', req.params.id);
+  const itemFound = items.find(item => item.id == req.params.id);
+  if (itemFound) {
+    return res.json(itemFound);
+  } else {
+    return res.status(404).json({message: 'Item not found'});
+  }
+});
+// TODO: add PUT Route for items
+// TODO: add DELETE Route for items
+
+// Add a new item
+app.post('/items', (req, res) => {
+    // console.log('add item request body:', req.body);
+    // TODO: Lisää id listaan lisättällä objektille
+    items.push(req.body);
+    res.status(201).json({message: 'Item added'});
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
